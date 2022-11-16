5059fd0c6c622df26ceac46ddf2b406ebbef3065
justamitsaha <justamitsaha@gmail.com>
2022-11-16 19:35:16 +0530

    Some changes

---
+++ src/com/saha/amit/L_Lambda/B_Lambda.java
@@ -0,0 +1,18 @@
+package com.saha.amit.L_Lambda;
+
+public class B_Lambda {
+	
+
+	public static void main(String[] args) {
+		String st = "Hello ";
+		TestInterface tst = s -> {
+			//st = "Hello !!";
+			return st + s;
+		};
+		System.out.println(tst.test("World"));
+	}
+}
+
+interface TestInterface {
+	String test(String st);
+}
